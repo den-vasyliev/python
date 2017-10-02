0cfc29fef31baea89fa3baea33d90e517110caa7
Denys Vasyliev <den@msrn.me>
2017-10-02 03:50:07 +0300

    add ingress

--- kub.py
+++ kub.py
@@ -1,16 +1,16 @@
 from flask import Flask
-from redis import Redis
+#from redis import Redis
 import os
 
 app = Flask(__name__)
-redis = Redis(host=os.getenv("REDIS_HOST", 'redis'), port=6379)
+#redis = Redis(host=os.getenv("REDIS_HOST", 'redis'), port=6379)
 
 
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return 'Hello %s\n' % redis.get('hits')
-
+ #   redis.incr('hits')
+  #  return 'Hello %s\n' % redis.get('hits')
+     return '200\n'
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0", debug=True)
